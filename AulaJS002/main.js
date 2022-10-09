eb838de9e5f7f49ae23731b26a4ab780e88caf8f
HELOISA BEATRIZ DE OLIVEIRA COSTA <P0649973@portoseguro.com.br>
2022-10-09 20:01:02 -0300

    AddJS

---
+++ AulaJS002/main.js
@@ -0,0 +1,25 @@
+/* */
+
+var B =  10;
+var A = 5;
+var C;
+
+C = A
+A = B
+
+
+console.log("A: " + A + " B: " + B + " C: " + C);
+
+/*O valor de B é: 5 O valor de A é: 10*/
+
+
+var nota1;
+var nota2;
+var nota3;
+var nota4;
+
+var media;
+
+media = nota1 + nota2 + nota3 + nota4 / 4
+
+console.log(media);
