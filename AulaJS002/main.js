d06dac02acae2c3583b337843f2fb4aa5c2ae20c
HELOISA BEATRIZ DE OLIVEIRA COSTA <P0649973@portoseguro.com.br>
2022-10-12 00:10:09 -0300

    Exercicio da aula 03 String

--- AulaJS002/main.js
+++ AulaJS002/main.js
@@ -1,42 +1,40 @@
-// let nota1 = 8;
-/* */
+// // let nota1 = 8;
+// /* */
 
-var B =  10;
-var A = 5;
-var C;
+// var B =  10;
+// var A = 5;
+// var C;
 
-C = A
-A = B
-
-
-console.log("A: " + A + " B: " + B + " C: " + C);
-
-/*O valor de B é: 5 O valor de A é: 10*/
+// C = A
+// A = B
 
 
-var nota1;
-var nota2;
-var nota3;
-var nota4;
+// console.log("A: " + A + " B: " + B + " C: " + C);
 
-var media;
-
-media = nota1 + nota2 + nota3 + nota4 / 4
-
-console.log(media);
-var teste = "Neste sentido, o início da atividade geral de formação 'de atitudes cumpre um papel essencial na formulação do investimento em reciclagem técnica."
-
-// var nota2 = 5;
-
-// var nota3 = 7;
-
-// var nota4 = 10;
-
-// var media = (nota1 + nota2 + nota3 + nota4) / 4;
-
-// console.log(`A media dos alunos é ${media}`)
+// /*O valor de B é: 5 O valor de A é: 10*/
 
 
+// var nota1;
+// var nota2;
+// var nota3;
+// var nota4;
+
+// var media;
+
+// media = nota1 + nota2 + nota3 + nota4 / 4
+
+// console.log(media);
+// var teste = "Neste sentido, o início da atividade geral de formação 'de atitudes cumpre um papel essencial na formulação do investimento em reciclagem técnica."
+
+// // var nota2 = 5;
+
+// // var nota3 = 7;
+
+// // var nota4 = 10;
+
+// // var media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// // console.log(`A media dos alunos é ${media}`)
 
 
 
@@ -46,35 +44,54 @@ var teste = "Neste sentido, o início da atividade geral de formação 'de atitu
 
 
 
-var nota = 5
-
-console.log("Gabiela sem Camisa " + 'Mayara sem Camisa ' + "Alunos sem cracha")
-console.log(`Quando a Gabriela está sem a camisa IOS eu vou dar nota ${nota}`)
 
 
-console.clear()
+// var nota = 5
+
+// console.log("Gabiela sem Camisa " + 'Mayara sem Camisa ' + "Alunos sem cracha")
+// console.log(`Quando a Gabriela está sem a camisa IOS eu vou dar nota ${nota}`)
+
+
+// console.clear()
 
 
 
-// Acessando um caracter de uma String
-var string1 = "Heloisa IOS";
-console.log(string1.length);
+// // Acessando um caracter de uma String
+// var string1 = "Heloisa IOS";
+// console.log(string1.length);
 
 
-// console.log(string1[0])
-// console.log(string1.charAt(3))
+// // console.log(string1[0])
+// // console.log(string1.charAt(3))
 
 
-//var
+// //var
 
-let sala = 10;
+// let sala = 10;
 
-let sala1 = 5;
+// let sala1 = 5;
 
-sala1 = 30;
+// sala1 = 30;
 
-//const
+// //const
 
-const PI = 3.14
+// const PI = 3.14
 
-PI = 5;
+// PI = 5;
+
+
+//Crie uma string com o texto "Estamos aprendendo JavaScript e seremos futuros Devs"
+//Exiba a seguintes informações no console
+var string2 = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+//O caracter que está na posição 19
+console.log(string2.charAt(19));
+
+//O tamanho da String
+console.log(string2.length);
+
+//Toda a String em maiusculo
+console.log(string2.toUpperCase());
+
+//Apenas a palavra Devs
+console.log(string2.substring(48));
