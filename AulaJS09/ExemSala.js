0aa8a1426844a4c0cc0570a7b6fe8115b1c3958e
Helloisa22 <helloisa.betriz44@gmail.com>
2022-10-21 20:23:21 -0300

    Aula JS09 Finalizada

--- AulaJS09/ExemSala.js
+++ AulaJS09/ExemSala.js
@@ -9,3 +9,75 @@ function minhaFuncao(valor, indice){
     console.log(`O valor é ${valor}`)
     console.log(`O indice é ${indice}`)
 }
+
+/*Metodo ForEach() com Array de Objetos 
+utilizando Arrow Function */
+const alunos =[
+    {
+        id: 1,
+        nome: "Marcelo",
+        passou: true,
+    },
+    {
+        id: 2,
+        nome: "Agatha",
+        passou: false,
+    },
+    {
+        id: 3,
+        nome: "Vitor",
+        passou: false,
+    }
+]
+
+alunos.forEach((aprovados) => console.log(aprovados.passou))
+
+/*Metodo map() */
+const numeros = [2, 7, 8, 50, 34];
+const novosNumeros = numeros.map(Math.sqrt);
+console.log(novosNumeros)
+
+console.clear()
+const cachorro = [
+    {
+        id: 1,
+        nome: "Bololinho",
+        idade: 11,
+    },
+    {
+        id: 2,
+        nome: "Toby",
+        idade: 7,
+    },
+    {
+        id: 3,
+        nome: "Pingo",
+        idade: 6,
+    },
+]
+
+const mapCachorro = cachorro.map((nomeDog) => {
+    return nomeDog.idade;
+})
+
+console.log(cachorro)
+console.log(mapCachorro)
+
+console.clear()
+/*Metodo filter() percorre o array e 
+retorna as informações que passsam no filtro */
+const idade = [5, 9, 10, 15, 25, 35, 45, 89]
+/*Percorrer o array e retorna apenas 
+as idades abaixo de 30 */
+let filtraIdade = idade.filter((idade) => {
+    return idade < 30
+})
+console.log(filtraIdade)
+
+console.clear()
+/*Metodo find() */
+console.log(
+    cachorro.find((procuraNome) => {
+        return procuraNome.id == 1;
+    })
+)
