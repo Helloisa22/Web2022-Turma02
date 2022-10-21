4b52f3a4f72867b8c7644cfd2393c231e31e3f82
HELOISA BEATRIZ DE OLIVEIRA COSTA <P0649973@portoseguro.com.br>
2022-10-21 09:45:22 -0300

    Termino aula JS 09

--- AulaJS09/pratica.js
+++ AulaJS09/pratica.js
@@ -31,3 +31,39 @@ const verb = [
 verb.forEach((teste) => console.log(teste.nameVerb))
 
 /*Método map() */
+var number = [10, 60, 17, 20, 40, 50, 70];
+const newArray = number.map(Math.sqrt);
+console.log("Array Antigo")
+console.log(number);
+console.log("Novo Array")
+console.log(newArray);
+
+/*Utilizando Arrow Function */
+const newArray2 = verb.map((cont) => {
+    return cont.nameVerb;
+})
+console.log(newArray2)
+
+
+/*Método filter() com uma Arrow Function*/
+var number2 = [2, 3, 5, 10, 90, 76]
+const newArray3 = number2.filter((cont2) => {
+    return cont2 < 8;
+})
+
+console.log(newArray3)
+
+const newArray4 = verb.filter((cont3) => {
+    return cont3.isVerb === true;
+})
+
+console.log('Retorna apenas os objetos que são verbos')
+console.log(newArray4)
+
+/*Método find() vamos retornar e utilizar o metodo filter direto no console.log */
+console.log(
+    newArray5 = verb.find((cont4) => {
+        return cont4.nameVerb === "Mean";
+    })
+)
+
