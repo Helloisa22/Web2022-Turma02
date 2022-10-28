f4d398f81dfd389c78b1e463d7c8fa4c9b596f82
HELOISA BEATRIZ DE OLIVEIRA COSTA <P0649973@portoseguro.com.br>
2022-10-28 11:53:41 -0300

    Nova funcionalidade de limpar o span e remover o valor do input

--- AulaJSDOM03/ExerAlone/JS/main.js
+++ AulaJSDOM03/ExerAlone/JS/main.js
@@ -1,9 +1,12 @@
 let tabuada = document.querySelector('.my-span'); // Pegar o Span
 let reserva = document.querySelector('#numero'); // Pegar o numero dentro input
 
-const Clique = () => {
+const Calcular = () => {
     let numeroTabuada = reserva.value;
-    tabuada.innerHTML = `   ${numeroTabuada} x 2 = ${numeroTabuada * 0}
+    if(numeroTabuada == ''){
+        alert("Insira um valor")
+    }else{
+        tabuada.innerHTML = `   ${numeroTabuada} x 2 = ${numeroTabuada * 0}
                             ${numeroTabuada} x 2 = ${numeroTabuada * 1}
                             ${numeroTabuada} x 2 = ${numeroTabuada * 2}
                             ${numeroTabuada} x 2 = ${numeroTabuada * 3}
@@ -14,8 +17,12 @@ const Clique = () => {
                             ${numeroTabuada} x 2 = ${numeroTabuada * 8}
                             ${numeroTabuada} x 2 = ${numeroTabuada * 9}
                             ${numeroTabuada} x 2 = ${numeroTabuada * 10}`
+    }
     console.log(numeroTabuada)
 }
 
-/*Até aqui será com o aluno */
+const Limpar = () => {
+    document.querySelector('.my-span').innerHTML="";
+    document.querySelector('#numero').value="";
+}
 
